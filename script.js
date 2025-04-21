73521abf672845e6795a828ac1a2858caca2041b
Enricobusatto <enricobusatto75@gmail.com>
2025-04-21 23:11:22 +0200

    ok

--- script.js
+++
@@ -1,9 +0,0 @@
-//fai funzionare la calcolatrice
-const display = document.getElementById('display');
-const buttons = document.querySelectorAll('.button');
-let currentInput = '';
-
-function appendToDisplay(value) {
-    currentInput += value;
-    display.value = currentInput;
-}
